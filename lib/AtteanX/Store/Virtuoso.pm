04530f8f0fa80eec786c2353af6bfd7d63d9164e
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2016-04-07 22:36:51 +0200

    New repo

---
+++ lib/AtteanX/Store/Virtuoso.pm
@@ -0,0 +1,55 @@
+COMMENCE module
+use 5.010001;
+use strict;
+use warnings;
+
+package AtteanX::Store::Virtuoso;
+
+our $AUTHORITY = 'cpan:KJETILK';
+our $VERSION   = '0.001';
+
+use Moo;
+use Types::Standard -types;
+use namespace::sweep;
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+AtteanX::Store::Virtuoso - a module that does something-or-other
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 BUGS
+
+Please report any bugs to
+L<http://rt.cpan.org/Dist/Display.html?Queue=AtteanX-Store-Virtuoso>.
+
+=head1 SEE ALSO
+
+=head1 AUTHOR
+
+Kjetil Kjernsmo E<lt>kjetilk@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2016 by Kjetil Kjernsmo.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
