a9c415370cbce10abebcae76ec97b5db5aeb46d5
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2016-04-08 15:17:13 +0200

    Read turtle from file

--- lib/AtteanX/Store/Virtuoso.pm
+++ lib/AtteanX/Store/Virtuoso.pm
@@ -12,6 +12,8 @@ use Types::Standard -types;
 use namespace::sweep;
 use Carp;
 use DBI;
+use Attean;
+use Attean::RDF;
 
 with 'Attean::API::QuadStore';
 with 'MooX::Log::Any';
@@ -20,21 +22,31 @@ has 'dsn' => (is => 'ro', required => 1, isa => Str);
 has 'dbuser' => (is => 'ro', isa => Str, default => 'dba');
 has 'dbpasswd' => (is => 'ro', isa => Str, default => 'dba');
 
-has '_dbh' => (is => 'lazy' isa => InstanceOf['DBI::db']);
+has '_dbh' => (is => 'lazy', isa => InstanceOf['DBI::db']);
 
-has 'turtle_files' => (is => 'lazy', isa => ArrayRef[Str]);
+has 'turtle_files' => (is => 'ro', isa => ArrayRef[Str]);
 
-has 'graph' => (is => 'ro', isa => InstanceOf['Attean::IRI'], coerce => 1);
+has 'graph' => (is => 'ro', isa => InstanceOf['Attean::IRI'], coerce => 1, default => sub { return iri('http://example.org/graph') });
 
-has 'base_uri' => (is => 'ro', isa => InstanceOf['Attean::IRI'], coerce => 1);
+has 'base_uri' => (is => 'ro', isa => InstanceOf['Attean::IRI'], predicate => 1, coerce => 1);
 
-has 'parse_flags' => (is => 'ro', isa => Int);
+has 'parse_flags' => (is => 'ro', isa => Int, predicate => 1, default => 512);
 
 sub BUILD {
 	my ($self, $args) = @_;
 	$self->_dbh; # Connect to the database
-	if ($args->{turtle_files}) {
-		$self->turtle_files;
+	foreach my $filename (@{$args->{turtle_files}}) {
+		my $sql = "DB.DBA.TTLP_MT (file_to_string_output ('$filename'), '";
+		if ($self->has_base_uri) {
+			$sql .= $self->base_uri->value;
+		}
+		$sql .= "', '". $self->graph->value ."'";
+		if ($self->has_parse_flags) {
+			$sql .= ", " . $self->parse_flags;
+		}
+		$sql .= ")";
+		warn $sql;
+		$self->_dbh->do($sql);
 	}
 }
 
@@ -49,13 +61,70 @@ sub _build__dbh {
 }
 
 
-sub _build_turtle_files {
-	my $self = shift;
-}
-
 sub get_quads {
 	my $self = shift;
+	my $sqlquery = <<'END';
+SELECT __id2i ( "s_1_1-t0"."S" ) AS "s",
+  1 AS "sisiri",
+  is_bnode_iri_id ( "s_1_1-t0"."S" ) AS "sisblank",
+  __id2i ( "s_1_1-t0"."P" ) AS "p",
+  __ro2sq ( "s_1_1-t0"."O" ) AS "o",
+  is_named_iri_id ( "s_1_1-t0"."O" ) AS "oisiri",
+  is_bnode_iri_id ( "s_1_1-t0"."O" ) AS "oisblank",
+  (1 - isiri_id ( "s_1_1-t0"."O" )) AS "oisliteral",
+  __rdf_sqlval_of_obj /*l*/ ( DB.DBA.RDF_DATATYPE_OF_OBJ (__ro2sq ( "s_1_1-t0"."O" ))) AS "datatype",
+  DB.DBA.RDF_LANGUAGE_OF_OBJ (__ro2sq ( "s_1_1-t0"."O" )) AS "lang",
+  __id2i ( "s_1_1-t0"."G" ) AS "g"
+FROM DB.DBA.RDF_QUAD AS "s_1_1-t0"
+OPTION (QUIETCAST)
+END
+	my $sth = $self->_dbh->prepare($sqlquery);
+		$sth->execute();
+		my $ok	= 1;
+		my $sub	= sub {
+			return unless ($ok);
+			if (my $row	= $sth->fetchrow_hashref) {
+				return $self->_get_quad(%$row);
+			}
+			$ok = 0;
+			return;
+		};
+		my $iter	= Attean::CodeIterator->new( generator => $sub, item_type => 'Attean::API::Quad' );
+		return $iter;
+
 }
+
+sub _get_quad {
+	my ($self, %row) = @_;
+	my $s;
+	my $p = iri($row{p});
+	my $o;
+	my $g = iri($row{g}) || $self->graph;
+	if ($row{sisiri}) {
+		$s = iri($row{s});
+	} elsif ($row{sisblank}) {
+		$s = blank($row{s});
+	} else {
+		croak "Subject $row{s} is neither IRI nor blank";
+	}
+	if ($row{oisiri}) {
+		$o = iri($row{o});
+	} elsif ($row{oisliteral}) {
+		if ($row{datatype}) {
+			$o = dtliteral($row{o}, $row{datatype});
+		} elsif ($row{lang}) {
+			$o = langliteral($row{o}, $row{lang});
+		} else {
+			$o = literal($row{o});
+		} # TODO: croak if both datatype and language
+	} elsif ($row{oisblank}) {
+		$o = blank($row{o});
+	} else {
+		croak "Subject $row{o} is neither IRI, literal nor blank";
+	}
+	return quad($s, $p, $o, $g);
+}
+
 
 1;
 
